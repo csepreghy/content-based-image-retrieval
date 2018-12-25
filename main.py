6eeabf732eb77644a2d615ed02664bea195368cf
Andrew Chepreghy <andris788@gmail.com>
2018-12-25 19:56:43 +0100

    corrected typo

--- main.py
+++ main.py
@@ -7,19 +7,18 @@ style.use('fivethirtyeight')
 # If you get an error regarding cv2.xfeatures2d it's because in the new version that algorithm isn't free
 # Do pip install opencv-contrib-python==3.4.1.15 to get rid of the error
 
-igm = cv2.imread("object_categories/brain/image_0003.jpg",
+img = cv2.imread("object_categories/brain/image_0003.jpg",
                  cv2.IMREAD_GRAYSCALE)
 
 sift = cv2.xfeatures2d.SIFT_create()
-(keypoints, descriptors) = sift.detectAndCompute(igm, None)
+(keypoints, descriptors) = sift.detectAndCompute(img, None)
 
-igm = cv2.drawKeypoints(igm, keypoints, None)
+img = cv2.drawKeypoints(img, keypoints, None)
 
-cv2.imshow("Image", igm)
+cv2.imshow("Image", img)
 cv2.waitKey(0)
-
 cv2.destroyAllWindows()
 
-print(igm)
+print(img)
 
 print("kps: {}, descriptors: {}".format(len(keypoints), descriptors.shape))
