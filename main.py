ab62639328213e384e2f510c673454f83da78994
Andrew Chepreghy <acsepreghy@ganintegrity.com>
2018-12-25 19:51:24 +0100

    refactor: changed to lowercase variable names

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ style.use('fivethirtyeight')
 # If you get an error regarding cv2.xfeatures2d it's because in the new version that algorithm isn't free
 # Do pip install opencv-contrib-python==3.4.1.15 to get rid of the error
 
-IMG = cv2.imread("object_categories/brain/image_0003.jpg",
+igm = cv2.imread("object_categories/brain/image_0003.jpg",
                  cv2.IMREAD_GRAYSCALE)
 
-SIFT = cv2.xfeatures2d.SIFT_create()
-(KEYPOINTS, DESCRIPTORS) = SIFT.detectAndCompute(IMG, None)
+sift = cv2.xfeatures2d.SIFT_create()
+(keypoints, descriptors) = sift.detectAndCompute(igm, None)
 
-IMG = cv2.drawKeypoints(IMG, KEYPOINTS, None)
+igm = cv2.drawKeypoints(igm, keypoints, None)
 
-cv2.imshow("Image", IMG)
+cv2.imshow("Image", igm)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
 
-print(IMG)
+print(igm)
 
-print("kps: {}, descriptors: {}".format(len(KEYPOINTS), DESCRIPTORS.shape))
+print("kps: {}, descriptors: {}".format(len(keypoints), descriptors.shape))
