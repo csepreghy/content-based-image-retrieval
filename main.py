acef5518ce8214e725f4575430145e89579758b4
Andrew Chepreghy <andris788@gmail.com>
2018-12-25 18:43:59 +0100

    feat: created basic SIFT keypoints

---
+++ main.py
@@ -0,0 +1,24 @@
+import cv2
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('fivethirtyeight')
+
+# If you get an error regarding cv2.xfeatures2d it's because in the new version that algorithm isn't free
+# Do pip install opencv-contrib-python==3.4.1.15 to get rid of the error
+
+IMG = cv2.imread("object_categories/brain/image_0004.jpg",
+                 cv2.IMREAD_GRAYSCALE)
+
+SIFT = cv2.xfeatures2d.SIFT_create()
+(KEYPOINTS, DESCRIPTORS) = SIFT.detectAndCompute(IMG, None)
+
+IMG = cv2.drawKeypoints(IMG, KEYPOINTS, None)
+
+cv2.imshow("Image", IMG)
+cv2.waitKey(0)
+
+
+print(IMG)
+
+print("kps: {}, descriptors: {}".format(len(KEYPOINTS), DESCRIPTORS.shape))
