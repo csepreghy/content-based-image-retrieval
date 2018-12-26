87e5987e543241d87e8ceabfdb02938970679a2b
Andrew Chepreghy <andris788@gmail.com>
2018-12-26 18:55:46 +0100

    refactor: number of images is now imported from a separate file

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
+from constants import NUM_OF_IMAGES
+
 import cv2
 import matplotlib.pyplot as plt
 from matplotlib import style
 
 style.use('fivethirtyeight')
 
-NUM_OF_IMAGES = 5
-keypoint_matrix = []
+descriptor_matrix = []
 
 # If you get an error regarding cv2.xfeatures2d it's because in the new version that algorithm isn't free
 # Do pip install opencv-contrib-python==3.4.1.15 to get rid of the error
@@ -33,14 +34,13 @@ for i in range(NUM_OF_IMAGES):
   keypoints, descriptors = sift.detectAndCompute(img, None)
   img = cv2.drawKeypoints(img, keypoints, None)
 
+  descriptor_matrix.append(descriptors)
+
   plt.imshow(img)
   plt.show()
   # cv2.imshow("Image", img)
   # cv2.waitKey(0)
   # cv2.destroyAllWindows()
 
-
-
-print(img)
 
 print("kps: {}, descriptors: {}".format(len(keypoints), descriptors.shape))
