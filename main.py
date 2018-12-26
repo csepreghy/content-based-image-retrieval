f7652773511533db895b6a0c09430b3c4983bb19
Andrew Chepreghy <acsepreghy@ganintegrity.com>
2018-12-26 18:48:09 +0100

    created loop that dynamically generates sift descriptors

--- main.py
+++ main.py
@@ -4,23 +4,42 @@ from matplotlib import style
 
 style.use('fivethirtyeight')
 
+NUM_OF_IMAGES = 5
+keypoint_matrix = []
+
 # If you get an error regarding cv2.xfeatures2d it's because in the new version that algorithm isn't free
 # Do pip install opencv-contrib-python==3.4.1.15 to get rid of the error
 
-img = cv2.imread("object_categories/brain/image_0003.jpg",
-                 cv2.IMREAD_GRAYSCALE)
-
 sift = cv2.xfeatures2d.SIFT_create()
-(keypoints, descriptors) = sift.detectAndCompute(img, None)
 
-img = cv2.drawKeypoints(img, keypoints, None)
+for i in range(NUM_OF_IMAGES):
+  print(i)
+  if i < 10:
+    img = cv2.imread("object_categories/brain/image_000" +
+                     str(i + 1) + ".jpg", cv2.IMREAD_GRAYSCALE)
 
-# cv2.imshow("Image", img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+  elif i >= 10 & i < 100:
+    img = cv2.imread("object_categories/brain/image_00" +
+                     str(i + 1) + ".jpg", cv2.IMREAD_GRAYSCALE)
 
-plt.imshow(img)
-plt.show()
+  elif i >= 100 & i < 1000:
+    img = cv2.imread("object_categories/brain/image_0" +
+                     str(i + 1) + ".jpg", cv2.IMREAD_GRAYSCALE)
+
+  elif i >= 100 & i < 10000:
+    img = cv2.imread("object_categories/brain/image_" +
+                     str(i + 1) + ".jpg", cv2.IMREAD_GRAYSCALE)
+
+  keypoints, descriptors = sift.detectAndCompute(img, None)
+  img = cv2.drawKeypoints(img, keypoints, None)
+
+  plt.imshow(img)
+  plt.show()
+  # cv2.imshow("Image", img)
+  # cv2.waitKey(0)
+  # cv2.destroyAllWindows()
+
+
 
 print(img)
 
