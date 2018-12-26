efb0c8fafda796ae7262e78b432433d6b155e2f4
Andrew Chepreghy <acsepreghy@ganintegrity.com>
2018-12-26 19:28:11 +0100

    added imports file, refactored descriptor generation

---
+++ src/import_modules.py
@@ -0,0 +1,8 @@
+# Our own files
+from constants import NUM_OF_IMAGES
+from sift_functions import get_descriptor_matrix
+
+# Python modules
+import cv2
+import matplotlib.pyplot as plt
+from matplotlib import style
