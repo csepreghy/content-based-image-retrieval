d252a77085ac04bbc09256a1d88becb1c959a16f
Andrew Chepreghy <acsepreghy@ganintegrity.com>
2018-12-26 19:35:12 +0100

    fix: invalid syntax error fix

--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@ from import_modules import *
 
 style.use('fivethirtyeight') # for matplotlib
 
-descriptor_matrix = get_descriptor_matrix(NUM_OF_IMAGES, "hello")
-print(len(descriptor_matrix))
+descriptor_matrix = get_descriptor_matrix(NUM_OF_IMAGES, "brain")
+# didn't make brain into a constant because we might have a list instead to loop througj
+
+print(descriptor_matrix)
 
 # If you get an error regarding cv2.xfeatures2d it's because in the new version that algorithm isn't free
 # Do pip install opencv-contrib-python==3.4.1.15 to get rid of the error
