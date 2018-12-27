563aabbf62f5aa258f4e6a2fd84d8f0606c245c5
Andrew Chepreghy <andris788@gmail.com>
2018-12-27 17:02:37 +0100

    renamed constant and fixed error on bigger n_images values

--- src/sift_functions.py
+++ src/sift_functions.py
@@ -5,36 +5,35 @@ from matplotlib import style
 
 style.use('fivethirtyeight')
 
-def get_descriptor_matrix(NUM_OF_IMAGES, img_category):
+def get_descriptor_matrix(N_IMAGES, img_category):
   sift = cv2.xfeatures2d.SIFT_create()
   descriptor_matrix = []
 
-  print(NUM_OF_IMAGES)
-  for i in range(NUM_OF_IMAGES):
+  print(N_IMAGES)
+  for i in range(N_IMAGES):
     print(i)
-    if i < 10:
+    if i < 9:
       img = cv2.imread("object_categories/" + img_category + "/image_000" +
                        str(i + 1) + ".jpg", cv2.IMREAD_GRAYSCALE)
 
-    elif i >= 10 & i < 100:
+    elif i >= 9 & i < 99:
       img = cv2.imread("object_categories/" + img_category + "/image_00" +
                        str(i + 1) + ".jpg", cv2.IMREAD_GRAYSCALE)
 
-    elif i >= 100 & i < 1000:
+    elif i >= 99 & i < 999:
       img = cv2.imread("object_categories/" + img_category + "/image_0" +
                        str(i + 1) + ".jpg", cv2.IMREAD_GRAYSCALE)
 
-    elif i >= 100 & i < 10000:
-      img = cv2.imread("object_categories/" + img_category + "/image_" +
-                       str(i + 1) + ".jpg", cv2.IMREAD_GRAYSCALE)
+    try:
+      keypoints, descriptors = sift.detectAndCompute(img, None)
+      img = cv2.drawKeypoints(img, keypoints, None)
+      descriptor_matrix.append(descriptors)
+    
+    except:
+      break
 
-    keypoints, descriptors = sift.detectAndCompute(img, None)
-    img = cv2.drawKeypoints(img, keypoints, None)
-
-    descriptor_matrix.append(descriptors)
-
-    plt.imshow(img)
-    plt.show()
+    # plt.imshow(img)
+    # plt.show()
     # cv2.imshow("Image", img)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
