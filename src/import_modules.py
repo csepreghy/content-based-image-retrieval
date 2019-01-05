3c804059409913eb2a1c682fa9f7fbcea9dbdd28
Andrew Chepreghy <andris788@gmail.com>
2019-01-05 13:24:10 +0100

    get descriptors to all categories function

--- src/import_modules.py
+++ src/import_modules.py
@@ -5,8 +5,9 @@ from matplotlib import style
 from os import listdir
 from sklearn.cluster import KMeans
 import numpy as np
+import pickle
 
 # Our own files
 from constants import N_IMAGES
-from sift_functions import get_descriptor_matrix
+from sift_functions import get_descriptor_matrix, get_descriptor_matrix_all_categories
 from helper_functions import get_all_categories
