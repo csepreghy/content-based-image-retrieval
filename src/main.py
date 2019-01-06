a5b41b3224b29aa8c4bf04869f61ad0cf415d2e4
Andrew Chepreghy <acsepreghy@ganintegrity.com>
2019-01-06 13:31:11 +0100

    k-means creates clusters from descriptors

--- src/main.py
+++ src/main.py
@@ -5,31 +5,32 @@ style.use('fivethirtyeight') # for matplotlib
 all_categories = get_all_categories()
 
 
-descriptor_matrix_10_10 = get_descriptor_matrix_10_10(all_categories)
+# descriptor_matrix_10_10 = get_descriptor_matrix_10_10(all_categories)
 
 # Write a variable into the pickle file:
 
-with open('descriptor_matrix_10_10.pickle', 'wb') as handle:
-    pickle.dump(descriptor_matrix_10_10,
-                handle, protocol=pickle.HIGHEST_PROTOCOL)
+# with open('descriptor_matrix_10_10.pickle', 'wb') as handle:
+#     pickle.dump(descriptor_matrix_10_10,
+#                 handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 # Read from the pickle file and assign it to a variable
 
-# with open('descriptor_matrix_10_10.pickle', 'rb') as handle:
-#     descriptor_matrix_10_10 = pickle.load(handle)
-#     print(len(descriptor_matrix_10_10))
+with open('descriptor_matrix_10_10.pickle', 'rb') as handle:
+    descriptor_matrix_10_10 = pickle.load(handle)
+    print(len(descriptor_matrix_10_10))
 
+# km = KMeans(init='k-means++', n_clusters=200, random_state=0, n_init=10).fit(np.array(descriptor_matrix_10_10))
 
-# descriptor_matrix_10_10 = get_descriptor_matrix_10_10(kmeans=KMeans(
-#     n_clusters=100, random_state=0).fit(np.array(descriptor_matrix_10_10)))
+kmeans = KMeans(n_clusters=200)
+kmeans.fit(np.array(descriptor_matrix_10_10))
 
 # print("kmeans labels")
-# print(kmeans.labels_)
+print(kmeans.labels_)
 
 # kmeans.predict()
 
 # print("cluster centers")
-# print(kmeans.cluster_centers_)
+print(kmeans.cluster_centers_)
 
 # If you get an error regarding cv2.xfeatures2d it's because in the new version that algorithm isn't free
 # Do pip install opencv-contrib-python==3.4.1.15 to get rid of the error
