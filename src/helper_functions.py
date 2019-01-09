9bfc5a37e83cb7b5644c30dc2aa006dfdca26e24
chepreghy <andris788@gmail.com>
2019-01-09 11:04:24 +0100

    code

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,7 +1,7 @@
 from import_modules import *
 
 def get_all_categories():
-  categories = [category for category in listdir("./object_categories")]
+  categories = [category for category in listdir("../object_categories")]
   return categories
 
 
